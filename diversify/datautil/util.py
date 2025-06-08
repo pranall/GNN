a44342049654e76d3d5deeee4353f5a109d318bb
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-08 13:19:54 +0530

    Update util.py

--- diversify/datautil/util.py
+++ diversify/datautil/util.py
@@ -1,122 +1,58 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-
+from torchvision import transforms
 import numpy as np
 import torch
+from torch.utils.data import Dataset
 
+def act_train():
+    return transforms.Compose([
+        transforms.ToTensor()
+    ])
 
-def Nmax(args, d):
-    for i in range(len(args.test_envs)):
-        if d < args.test_envs[i]:
-            return i
-    return len(args.test_envs)
+def loaddata_from_numpy(dataset='dsads', task='cross_people', root_dir='./data/act/'):
+    if dataset == 'pamap' and task == 'cross_people':
+        x = np.load(root_dir + dataset + '/' + dataset + '_x1.npy')
+        ty = np.load(root_dir + dataset + '/' + dataset + '_y1.npy')
+    else:
+        x = np.load(root_dir + dataset + '/' + dataset + '_x.npy')
+        ty = np.load(root_dir + dataset + '/' + dataset + '_y.npy')
+    cy, py, sy = ty[:, 0], ty[:, 1], ty[:, 2]
+    return x, cy, py, sy
 
+class subdataset(Dataset):
+    def __init__(self, dataset, indices, transform=None):
+        indices = torch.LongTensor(indices)  # ✅ Fix: ensure indices are torch.LongTensor
 
-class basedataset(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __getitem__(self, index):
-        return self.x[index], self.y[index]
-
-    def __len__(self):
-        return len(self.x)
-
-
-class mydataset(object):
-    def __init__(self, args):
-        self.x = None
-        self.labels = None
-        self.dlabels = None
-        self.pclabels = None
-        self.pdlabels = None
-        self.task = None
-        self.dataset = None
-        self.transform = None
-        self.target_transform = None
-        self.loader = None
-        self.args = args
-
-    def set_labels(self, tlabels=None, label_type='domain_label'):
-        assert len(tlabels) == len(self.x)
-        if label_type == 'pclabel':
-            self.pclabels = tlabels
-        elif label_type == 'pdlabel':
-            self.pdlabels = tlabels
-        elif label_type == 'domain_label':
-            self.dlabels = tlabels
-        elif label_type == 'class_label':
-            self.labels = tlabels
-
-    def set_labels_by_index(self, tlabels=None, tindex=None, label_type='domain_label'):
-        if label_type == 'pclabel':
-            self.pclabels[tindex] = tlabels
-        elif label_type == 'pdlabel':
-            self.pdlabels[tindex] = tlabels
-        elif label_type == 'domain_label':
-            self.dlabels[tindex] = tlabels
-        elif label_type == 'class_label':
-            self.labels[tindex] = tlabels
-
-    def target_trans(self, y):
-        if self.target_transform is not None:
-            return self.target_transform(y)
-        else:
-            return y
-
-    def input_trans(self, x):
-        if self.transform is not None:
-            return self.transform(x)
-        else:
-            return x
-
-    def __getitem__(self, index):
-        x = self.input_trans(self.x[index])
-
-        ctarget = self.target_trans(self.labels[index])
-        dtarget = self.target_trans(self.dlabels[index])
-        pctarget = self.target_trans(self.pclabels[index])
-        pdtarget = self.target_trans(self.pdlabels[index])
-        return x, ctarget, dtarget, pctarget, pdtarget, index
-
-    def __len__(self):
-        return len(self.x)
-
-
-class subdataset(mydataset):
-    def __init__(self, args, dataset, indices):
-        super(subdataset, self).__init__(args)
         self.x = dataset.x[indices]
-        self.loader = dataset.loader
         self.labels = dataset.labels[indices]
+
         self.dlabels = dataset.dlabels[indices] if dataset.dlabels is not None else None
         self.pclabels = dataset.pclabels[indices] if dataset.pclabels is not None else None
         self.pdlabels = dataset.pdlabels[indices] if dataset.pdlabels is not None else None
-        self.task = dataset.task
-        self.dataset = dataset.dataset
-        self.transform = dataset.transform
-        self.target_transform = dataset.target_transform
 
+        self.index = indices
+        self.transform = transform
 
-class combindataset(mydataset):
-    def __init__(self, args, datalist):
-        super(combindataset, self).__init__(args)
-        self.domain_num = len(datalist)
-        self.loader = datalist[0].loader
-        xlist = [item.x for item in datalist]
-        cylist = [item.labels for item in datalist]
-        dylist = [item.dlabels for item in datalist]
-        pcylist = [item.pclabels for item in datalist]
-        pdylist = [item.pdlabels for item in datalist]
-        self.dataset = datalist[0].dataset
-        self.task = datalist[0].task
-        self.transform = datalist[0].transform
-        self.target_transform = datalist[0].target_transform
-        self.x = torch.vstack(xlist)
+    def __getitem__(self, i):
+        x = self.x[i]
+        if self.transform:
+            x = self.transform(x)
+        label = self.labels[i]
+        dlabel = self.dlabels[i] if self.dlabels is not None else -1
+        pclabel = self.pclabels[i] if self.pclabels is not None else -1
+        pdlabel = self.pdlabels[i] if self.pdlabels is not None else -1
+        return x, label, dlabel, pclabel, pdlabel, self.index[i]
 
-        self.labels = np.hstack(cylist)
-        self.dlabels = np.hstack(dylist)
-        self.pclabels = np.hstack(pcylist) if pcylist[0] is not None else None
-        self.pdlabels = np.hstack(pdylist) if pdylist[0] is not None else None
+    def __len__(self):
+        return len(self.labels)
+
+    def set_labels_by_index(self, tlabels, tindex, type='pdlabel'):
+        tindex = torch.LongTensor(tindex)
+        if type == 'pdlabel':
+            self.pdlabels[tindex] = tlabels
+        elif type == 'pclabel':
+            self.pclabels[tindex] = tlabels
+        elif type == 'dlabel':
+            self.dlabels[tindex] = tlabels
