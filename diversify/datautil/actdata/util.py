dd6fbc65b6f3df6a52db5e80677c2b364c185d44
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-18 09:23:52 +0530

    Update util.py

--- diversify/datautil/actdata/util.py
+++ diversify/datautil/actdata/util.py
@@ -1,200 +1,23 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import random
+
+from torchvision import transforms
 import numpy as np
-import torch
-import sys
-import os
-import argparse
-import torchvision
-import PIL
-from torch.utils.data import Dataset
 
 
-def set_random_seed(seed=0):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+def act_train():
+    return transforms.Compose([
+        transforms.ToTensor()
+    ])
 
 
-def train_valid_target_eval_names(args):
-    eval_name_dict = {'train': [], 'valid': [], 'target': []}
-    for i in range(args.domain_num):
-        if i not in args.test_envs:
-            eval_name_dict['train'].append('eval%d_in' % i)
-            eval_name_dict['valid'].append('eval%d_out' % i)
-        else:
-            eval_name_dict['target'].append('eval%d_out' % i)
-    return eval_name_dict
-
-
-def alg_loss_dict(args):
-    loss_dict = {'diversify': ['class', 'dis', 'total']}
-    return loss_dict[args.algorithm]
-
-
-def print_args(args, print_list):
-    s = "==========================================\n"
-    l = len(print_list)
-    for arg, content in args.__dict__.items():
-        if l == 0 or arg in print_list:
-            s += "{}:{}\n".format(arg, content)
-    return s
-
-
-def print_row(row, colwidth=10, latex=False):
-    if latex:
-        sep = " & "
-        end_ = "\\\\"
+def loaddata_from_numpy(dataset='dsads', task='cross_people', root_dir='./data/act/'):
+    if dataset == 'pamap' and task == 'cross_people':
+        x = np.load(root_dir+dataset+'/'+dataset+'_x1.npy')
+        ty = np.load(root_dir+dataset+'/'+dataset+'_y1.npy')
     else:
-        sep = "  "
-        end_ = ""
-
-    def format_val(x):
-        if np.issubdtype(type(x), np.floating):
-            x = "{:.10f}".format(x)
-        return str(x).ljust(colwidth)[:colwidth]
-    print(sep.join([format_val(x) for x in row]), end_)
-
-
-def print_environ():
-    print("Environment:")
-    print("\tPython: {}".format(sys.version.split(" ")[0]))
-    print("\tPyTorch: {}".format(torch.__version__))
-    print("\tTorchvision: {}".format(torchvision.__version__))
-    print("\tCUDA: {}".format(torch.version.cuda))
-    print("\tCUDNN: {}".format(torch.backends.cudnn.version()))
-    print("\tNumPy: {}".format(np.__version__))
-    print("\tPIL: {}".format(PIL.__version__))
-
-
-class Tee:
-    def __init__(self, fname, mode="a"):
-        self.stdout = sys.stdout
-        self.file = open(fname, mode)
-
-    def write(self, message):
-        self.stdout.write(message)
-        self.file.write(message)
-        self.flush()
-
-    def flush(self):
-        self.stdout.flush()
-        self.file.flush()
-
-
-def act_param_init(args):
-    args.select_position = {'emg': [0]}
-    args.select_channel = {'emg': np.arange(8)}
-    args.hz_list = {'emg': 1000}
-    args.act_people = {'emg': [[i*9+j for j in range(9)] for i in range(4)]}
-    tmp = {'emg': ((8, 1, 200), 6, 10)}
-    args.num_classes, args.input_shape, args.grid_size = tmp[args.dataset][1], tmp[args.dataset][0], tmp[args.dataset][2]
-    return args
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description='DG')
-    parser.add_argument('--algorithm', type=str, default="diversify")
-    parser.add_argument('--alpha', type=float, default=0.1, help="DANN dis alpha")
-    parser.add_argument('--alpha1', type=float, default=0.1, help="DANN dis alpha")
-    parser.add_argument('--batch_size', type=int, default=32, help="batch_size")
-    parser.add_argument('--beta1', type=float, default=0.5, help="Adam")
-    parser.add_argument('--bottleneck', type=int, default=256)
-    parser.add_argument('--checkpoint_freq', type=int, default=100, help='Checkpoint every N steps')
-    parser.add_argument('--classifier', type=str, default="linear", choices=["linear", "wn"])
-    parser.add_argument('--data_file', type=str, default='')
-    parser.add_argument('--dataset', type=str, default='dsads')
-    parser.add_argument('--data_dir', type=str, default='')
-    parser.add_argument('--dis_hidden', type=int, default=256)
-    parser.add_argument('--gpu_id', type=str, nargs='?', default='0', help="device id to run")
-    parser.add_argument('--layer', type=str, default="bn", choices=["ori", "bn"])
-    parser.add_argument('--lam', type=float, default=0.0)
-    parser.add_argument('--latent_domain_num', type=int, default=3)
-    parser.add_argument('--local_epoch', type=int, default=1, help='local iterations')
-    parser.add_argument('--lr', type=float, default=1e-2, help="learning rate")
-    parser.add_argument('--lr_decay1', type=float, default=1.0, help='for pretrained featurizer')
-    parser.add_argument('--lr_decay2', type=float, default=1.0)
-    parser.add_argument('--max_epoch', type=int, default=120, help="max iterations")
-    parser.add_argument('--model_size', default='median', choices=['small', 'median', 'large', 'transformer'])
-    parser.add_argument('--N_WORKERS', type=int, default=4)
-    parser.add_argument('--old', action='store_true')
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--task', type=str, default="cross_people")
-    parser.add_argument('--test_envs', type=int, nargs='+', default=[0])
-    parser.add_argument('--output', type=str, default="train_output")
-    parser.add_argument('--weight_decay', type=float, default=5e-4)
-    args = parser.parse_args()
-    args.steps_per_epoch = 10000000000
-    args.data_dir = args.data_file + args.data_dir
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
-    os.makedirs(args.output, exist_ok=True)
-    sys.stdout = Tee(os.path.join(args.output, 'out.txt'))
-    sys.stderr = Tee(os.path.join(args.output, 'err.txt'))
-    args = act_param_init(args)
-    return args
-
-
-# === For Metrics + Loader Compatibility ===
-
-class combindataset(Dataset):
-    def __init__(self, args, datasetlist):
-        self.args = args
-        x_list, y_list, p_list, s_list = [], [], [], []
-        for ds in datasetlist:
-            x_list.append(ds.x)
-            y_list.append(ds.labels)
-            p_list.append(ds.people)
-            s_list.append(ds.sensors)
-        self.x = np.concatenate(x_list, axis=0)
-        self.labels = np.concatenate(y_list, axis=0)
-        self.people = np.concatenate(p_list, axis=0)
-        self.sensors = np.concatenate(s_list, axis=0)
-        self.pdlabels = np.zeros(len(self.x), dtype=np.int64)
-
-    def __len__(self):
-        return len(self.x)
-
-    def __getitem__(self, index):
-        x = self.x[index]
-        x = torch.tensor(x).float().unsqueeze(1)  # shape: (C, 1, T)
-        label = self.labels[index]
-        person = self.people[index]
-        sensor = self.sensors[index]
-        pdlabel = self.pdlabels[index]
-        return x, label, person, sensor, pdlabel, index
-
-
-class subdataset(Dataset):
-    def __init__(self, args, dataset, indices, transform=None):
-        self.args = args
-        self.dataset = dataset
-        self.indices = np.array(indices, dtype=np.int64)
-        self.transform = transform
-        self.pdlabels = dataset.pdlabels[indices]
-
-    def __len__(self):
-        return len(self.indices)
-
-    def __getitem__(self, idx):
-        real_idx = self.indices[idx]
-        return self.dataset[real_idx]
-
-class mydataset:
-    def __init__(self, args):
-        self.args = args
-        self.x = None
-        self.labels = None
-        self.dlabels = None
-        self.pclabels = None
-        self.pdlabels = None
-
-Nmax = {
-    'emg': 36,
-    'pamap': 10,
-    'dsads': 8
-}
+        x = np.load(root_dir+dataset+'/'+dataset+'_x.npy')
+        ty = np.load(root_dir+dataset+'/'+dataset+'_y.npy')
+    cy, py, sy = ty[:, 0], ty[:, 1], ty[:, 2]
+    return x, cy, py, sy
