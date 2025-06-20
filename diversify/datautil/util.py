e21ea68413c8001932c6487fd14899a9fe2cd380
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-20 13:47:39 +0530

    Update util.py

--- diversify/datautil/util.py
+++ diversify/datautil/util.py
@@ -1,122 +1,61 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-
 import numpy as np
 import torch
+from torch_geometric.data import Batch
 
+class GraphDatasetMixin:
+    """Mixin class for graph data support"""
+    def __init__(self):
+        self.edge_indices = None
+        self.batches = None
+        
+    def set_graph_attributes(self, edge_indices, batches):
+        self.edge_indices = edge_indices
+        self.batches = batches
 
-def Nmax(args, d):
-    for i in range(len(args.test_envs)):
-        if d < args.test_envs[i]:
-            return i
-    return len(args.test_envs)
-
-
-class basedataset(object):
+class basedataset(GraphDatasetMixin):
     def __init__(self, x, y):
+        super().__init__()
         self.x = x
         self.y = y
-
+        
     def __getitem__(self, index):
+        if self.edge_indices is not None:
+            return self.x[index], self.y[index], self.edge_indices[index]
         return self.x[index], self.y[index]
 
-    def __len__(self):
-        return len(self.x)
-
-
-class mydataset(object):
+class mydataset(GraphDatasetMixin):
     def __init__(self, args):
+        super().__init__()
         self.x = None
         self.labels = None
         self.dlabels = None
         self.pclabels = None
         self.pdlabels = None
-        self.task = None
-        self.dataset = None
-        self.transform = None
-        self.target_transform = None
-        self.loader = None
         self.args = args
-
-    def set_labels(self, tlabels=None, label_type='domain_label'):
-        assert len(tlabels) == len(self.x)
-        if label_type == 'pclabel':
-            self.pclabels = tlabels
-        elif label_type == 'pdlabel':
-            self.pdlabels = tlabels
-        elif label_type == 'domain_label':
-            self.dlabels = tlabels
-        elif label_type == 'class_label':
-            self.labels = tlabels
-
-    def set_labels_by_index(self, tlabels=None, tindex=None, label_type='domain_label'):
-        if label_type == 'pclabel':
-            self.pclabels[tindex] = tlabels
-        elif label_type == 'pdlabel':
-            self.pdlabels[tindex] = tlabels
-        elif label_type == 'domain_label':
-            self.dlabels[tindex] = tlabels
-        elif label_type == 'class_label':
-            self.labels[tindex] = tlabels
-
-    def target_trans(self, y):
-        if self.target_transform is not None:
-            return self.target_transform(y)
-        else:
-            return y
-
-    def input_trans(self, x):
-        if self.transform is not None:
-            return self.transform(x)
-        else:
-            return x
-
+        
     def __getitem__(self, index):
-        x = self.input_trans(self.x[index])
+        item = [
+            self.input_trans(self.x[index]),
+            self.target_trans(self.labels[index]),
+            self.target_trans(self.dlabels[index]),
+            self.target_trans(self.pclabels[index]) if self.pclabels is not None else -1,
+            self.target_trans(self.pdlabels[index]) if self.pdlabels is not None else -1,
+            index
+        ]
+        
+        if self.edge_indices is not None:
+            item.append(self.edge_indices[index])
+        return tuple(item)
 
-        ctarget = self.target_trans(self.labels[index])
-        dtarget = self.target_trans(self.dlabels[index])
-        pctarget = self.target_trans(self.pclabels[index])
-        pdtarget = self.target_trans(self.pdlabels[index])
-        return x, ctarget, dtarget, pctarget, pdtarget, index
-
-    def __len__(self):
-        return len(self.x)
-
-
-class subdataset(mydataset):
-    def __init__(self, args, dataset, indices):
-        super(subdataset, self).__init__(args)
-        self.x = dataset.x[indices]
-        self.loader = dataset.loader
-        self.labels = dataset.labels[indices]
-        self.dlabels = dataset.dlabels[indices] if dataset.dlabels is not None else None
-        self.pclabels = dataset.pclabels[indices] if dataset.pclabels is not None else None
-        self.pdlabels = dataset.pdlabels[indices] if dataset.pdlabels is not None else None
-        self.task = dataset.task
-        self.dataset = dataset.dataset
-        self.transform = dataset.transform
-        self.target_transform = dataset.target_transform
-
-
-class combindataset(mydataset):
-    def __init__(self, args, datalist):
-        super(combindataset, self).__init__(args)
-        self.domain_num = len(datalist)
-        self.loader = datalist[0].loader
-        xlist = [item.x for item in datalist]
-        cylist = [item.labels for item in datalist]
-        dylist = [item.dlabels for item in datalist]
-        pcylist = [item.pclabels for item in datalist]
-        pdylist = [item.pdlabels for item in datalist]
-        self.dataset = datalist[0].dataset
-        self.task = datalist[0].task
-        self.transform = datalist[0].transform
-        self.target_transform = datalist[0].target_transform
-        self.x = torch.vstack(xlist)
-
-        self.labels = np.hstack(cylist)
-        self.dlabels = np.hstack(dylist)
-        self.pclabels = np.hstack(pcylist) if pcylist[0] is not None else None
-        self.pdlabels = np.hstack(pdylist) if pdylist[0] is not None else None
+def graph_collate_fn(batch):
+    """Handle both graph and non-graph batches"""
+    if isinstance(batch[0], (tuple, list)) and len(batch[0]) > 6:  # Has edge_index
+        return Batch.from_data_list([Data(
+            x=item[0], 
+            y=item[1], 
+            edge_index=item[6],
+            domain=item[2],
+            pclabel=item[3],
+            pdlabel=item[4]
+        ) for item in batch])
+    return torch.utils.data.default_collate(batch)
