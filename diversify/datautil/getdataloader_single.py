b53a2e27f3604f88e375e453992db09c7834de28
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-25 12:03:42 +0530

    Update getdataloader_single.py

--- diversify/datautil/getdataloader_single.py
+++ diversify/datautil/getdataloader_single.py
@@ -1,11 +1,9 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
 import os
 import numpy as np
+import torch
 from torch.utils.data import DataLoader
 from torch_geometric.data import Batch
-from datautil.util import graph_collate_fn  # From updated util.py
+from datautil.util import graph_collate_fn
 import datautil.actdata.util as actutil
 from datautil.util import combindataset, subdataset
 import datautil.actdata.cross_people as cross_people
@@ -13,29 +11,32 @@ import datautil.actdata.cross_people as cross_people
 task_act = {'cross_people': cross_people}
 
 def get_dataloader(args, tr, val, tar):
-    """Enhanced data loader with GNN support"""
-    collate_fn = graph_collate_fn if args.algorithm.lower() == 'gnn' else None
+    """GNN-compatible data loader with dynamic batch handling"""
+    collate_fn = graph_collate_fn if args.use_gnn else None
     
-    loaders = (
-        DataLoader(
-            dataset=tr,
-            batch_size=args.batch_size,
-            num_workers=args.N_WORKERS,
-            shuffle=shuffle,
-            drop_last=False,
-            collate_fn=collate_fn
-        )
-        for shuffle in [True, False]  # For train and train_noshuffle
+    # Train loaders (shuffled/non-shuffled)
+    train_loader = DataLoader(
+        dataset=tr,
+        batch_size=args.batch_size,
+        num_workers=args.N_WORKERS,
+        shuffle=True,
+        collate_fn=collate_fn
     )
     
-    train_loader, train_loader_noshuffle = loaders
-    
+    train_loader_noshuffle = DataLoader(
+        dataset=tr,
+        batch_size=args.batch_size,
+        num_workers=args.N_WORKERS,
+        shuffle=False,
+        collate_fn=collate_fn
+    )
+
+    # Validation and target loaders
     valid_loader = DataLoader(
         dataset=val,
         batch_size=args.batch_size,
         num_workers=args.N_WORKERS,
         shuffle=False,
-        drop_last=False,
         collate_fn=collate_fn
     )
     
@@ -44,75 +45,49 @@ def get_dataloader(args, tr, val, tar):
         batch_size=args.batch_size,
         num_workers=args.N_WORKERS,
         shuffle=False,
-        drop_last=False,
         collate_fn=collate_fn
     )
     
-    return train_loader, train_loader_noshuffle, valid_loader, target_loader
+    return train_loader, train_loader_noshuffle, valid_loader, target_loader, tr, val, tar
 
 def get_act_dataloader(args):
-    """Main function to get EMG data loaders with GNN support"""
-    source_datasetlist = []
-    target_datalist = []
+    """Main loader with GNN graph conversion"""
+    source_datasets = []
+    target_datasets = []
     pcross_act = task_act[args.task]
     tmpp = args.act_people[args.dataset]
     args.domain_num = len(tmpp)
 
-    # Load and process datasets
+    # Load and convert datasets
     for i, item in enumerate(tmpp):
         data_root = os.path.abspath(args.data_dir)
-        tdata = pcross_act.ActList(
-            args, 
-            args.dataset, 
-            data_root, 
-            item, 
-            i, 
+        dataset = pcross_act.ActList(
+            args,
+            args.dataset,
+            data_root,
+            item,
+            i,
             transform=actutil.act_train()
         )
         
-        # Convert to graph if in GNN mode
-        if args.algorithm.lower() == 'gnn':
-            tdata.convert_to_graph(threshold=args.graph_threshold)
-            
+        if args.use_gnn:
+            dataset.convert_to_graph(
+                threshold=getattr(args, 'graph_threshold', 0.3),
+                sensor_layout='myo'  # MYO armband specific
+            )
+
         if i in args.test_envs:
-            target_datalist.append(tdata)
+            target_datasets.append(dataset)
         else:
-            source_datasetlist.append(tdata)
-            if len(tdata)/args.batch_size < args.steps_per_epoch:
-                args.steps_per_epoch = len(tdata)/args.batch_size
+            source_datasets.append(dataset)
 
-    # Prepare datasets
-    rate = 0.2
-    args.steps_per_epoch = int(args.steps_per_epoch*(1-rate))
+    # Combine datasets with validation split
+    combined_source = combindataset(args, source_datasets)
+    indices = np.random.permutation(len(combined_source))
+    val_size = int(0.2 * len(combined_source))
     
-    tdata = combindataset(args, source_datasetlist)
-    l = len(tdata.labels)
-    indexall = np.arange(l)
-    np.random.seed(args.seed)
-    np.random.shuffle(indexall)
-    
-    ted = int(l*rate)
-    indextr, indexval = indexall[ted:], indexall[:ted]
-    
-    tr = subdataset(args, tdata, indextr)
-    val = subdataset(args, tdata, indexval)
-    targetdata = combindataset(args, target_datalist)
-    
-    return get_dataloader(args, tr, val, targetdata) + (tr, val, targetdata)
+    train_data = subdataset(args, combined_source, indices[val_size:])
+    val_data = subdataset(args, combined_source, indices[:val_size])
+    target_data = combindataset(args, target_datasets)
 
-# Add this to your ActList class (in cross_people.py)
-class ActList:
-    # ... existing code ...
-    
-    def convert_to_graph(self, threshold=0.3):
-        """Convert EMG data to graph format"""
-        from datautil.actdata.util import emg_to_graph  # Avoid circular import
-        if not hasattr(self, 'x'):
-            return
-            
-        self.graphs = emg_to_graph(self.x, self.labels, threshold)
-        self.edge_indices = [g.edge_index for g in self.graphs]
-        self.batches = [g.batch if hasattr(g, 'batch') else None for g in self.graphs]
-        
-        # Update dataset attributes
-        self.set_graph_attributes(self.edge_indices, self.batches)
+    return get_dataloader(args, train_data, val_data, target_data)
