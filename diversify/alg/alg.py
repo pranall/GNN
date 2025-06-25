08a52fd3f3bda7fe9ef4900542ab1660fa52d66b
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-25 18:36:12 +0530

    Update alg.py

--- diversify/alg/alg.py
+++ diversify/alg/alg.py
@@ -1,11 +1,17 @@
-from algs.diversify import Diversify
+# diversify/alg/alg.py
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from .algs.diversify import Diversify
 
 ALGORITHMS = [
     'diversify'
 ]
 
 def get_algorithm_class(algorithm_name):
-    if algorithm_name not in ALGORITHMS:
+    name = algorithm_name.lower()
+    if name not in ALGORITHMS:
         raise NotImplementedError(
-            "Algorithm not found: {}".format(algorithm_name))
+            f"Algorithm not found: {algorithm_name}"
+        )
     return Diversify
