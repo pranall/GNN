1f4f79b88fe19ee06e60151b22432948f83b44a2
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-26 23:18:40 +0530

    Update opt.py

--- diversify/alg/opt.py
+++ diversify/alg/opt.py
@@ -1,31 +1,55 @@
 import torch
 
 def get_params(alg, args, nettype):
+    """
+    Get parameter groups with customized learning rates
+    Args:
+        alg: Algorithm/model instance
+        args: Configuration arguments
+        nettype: Network type identifier
+    Returns:
+        List of parameter groups with learning rate settings
+    """
     init_lr = args.lr
+    
     if nettype == 'Diversify-adv':
-        params = [
+        return [
             {'params': alg.dbottleneck.parameters(), 'lr': args.lr_decay2 * init_lr},
             {'params': alg.dclassifier.parameters(), 'lr': args.lr_decay2 * init_lr},
             {'params': alg.ddiscriminator.parameters(), 'lr': args.lr_decay2 * init_lr}
         ]
-        return params
+    
     elif nettype == 'Diversify-cls':
-        params = [
+        return [
             {'params': alg.bottleneck.parameters(), 'lr': args.lr_decay2 * init_lr},
             {'params': alg.classifier.parameters(), 'lr': args.lr_decay2 * init_lr},
             {'params': alg.discriminator.parameters(), 'lr': args.lr_decay2 * init_lr}
         ]
-        return params
+    
     elif nettype == 'Diversify-all':
-        params = [
+        return [
             {'params': alg.featurizer.parameters(), 'lr': args.lr_decay1 * init_lr},
             {'params': alg.abottleneck.parameters(), 'lr': args.lr_decay2 * init_lr},
             {'params': alg.aclassifier.parameters(), 'lr': args.lr_decay2 * init_lr}
         ]
-        return params
+    
+    # Default case: return all parameters with base learning rate
+    return [{'params': alg.parameters(), 'lr': init_lr}]
 
 def get_optimizer(alg, args, nettype):
-    params = get_params(alg, args, nettype=nettype)
-    optimizer = torch.optim.Adam(
-        params, lr=args.lr, weight_decay=args.weight_decay, betas=(args.beta1, 0.9))
-    return optimizer
+    """
+    Create optimizer for specified network components
+    Args:
+        alg: Algorithm/model instance
+        args: Configuration arguments
+        nettype: Network type identifier
+    Returns:
+        Configured Adam optimizer
+    """
+    params = get_params(alg, args, nettype)
+    return torch.optim.Adam(
+        params, 
+        lr=args.lr, 
+        weight_decay=args.weight_decay, 
+        betas=(args.beta1, 0.9)
+    )
