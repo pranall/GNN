d937b9c23d99dffdb6cf33db97fc54a67652a9c6
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-28 00:53:51 +0530

    Update train.py

--- diversify/train.py
+++ diversify/train.py
@@ -151,9 +151,9 @@ def main(args):
 
         # Evaluation
         acc_fn = modelopera.accuracy
-        logs['train_acc'].append(acc_fn(algorithm, train_ns_loader, args.device))
-        logs['val_acc'].append  (acc_fn(algorithm, val_loader, args.device))
-        logs['test_acc'].append (acc_fn(algorithm, test_loader, args.device))
+        logs['train_acc'].append(acc_fn(algorithm, train_ns_loader, device))
+        logs['val_acc'].append  (acc_fn(algorithm, val_loader, device))
+        logs['test_acc'].append (acc_fn(algorithm, test_loader, device))
 
         scheduler.step()
         if logs['val_acc'][-1] > best_val:
