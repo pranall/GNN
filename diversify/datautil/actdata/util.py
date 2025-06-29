79a21cbe032264049fea39a662f5cecf32dbb2cb
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-29 22:01:37 +0530

    Update util.py

--- diversify/datautil/actdata/util.py
+++ diversify/datautil/actdata/util.py
@@ -30,6 +30,7 @@ def act_to_graph_transform(args):
     """Transformation pipeline for GNN models"""
     def _to_graph(x):
         print("ORIGINAL x.shape:", x.shape)
+        print("INSIDE _to_graph, got x.shape:", x.shape, "| type:", type(x))
         if isinstance(x, torch.Tensor):
             if x.dim() == 3 and x.shape == (8, 1, 200):
                 x = x.squeeze(1).transpose(1, 0)
