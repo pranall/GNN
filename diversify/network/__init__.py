16a02b98a8fdd43096d0aaa5d1911c5a85307472
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-29 20:11:45 +0530

    Update __init__.py

--- diversify/network/__init__.py
+++ diversify/network/__init__.py
@@ -1,5 +1,3 @@
-from .act_network import ActNetwork
-from .Adver_network import AdversarialNetwork
-from .common_network import CommonNetwork
-
-__all__ = ['ActNetwork', 'AdversarialNetwork', 'CommonNetwork']
+from .act_network import *
+from .Adver_network import *
+from .common_network import *
