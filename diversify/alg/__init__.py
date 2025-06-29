444f4bbe6e900cd2d9b3bb3014224149e0a34d57
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-29 09:58:48 +0530

    Update __init__.py

--- diversify/alg/__init__.py
+++ diversify/alg/__init__.py
@@ -1,3 +1,5 @@
-from .alg import *
-from .modelopera import *
-from .opt import *
+from .alg import Diversify
+from .modelopera import get_fea
+from .opt import get_optimizer
+
+__all__ = ['Diversify', 'get_fea', 'get_optimizer']
