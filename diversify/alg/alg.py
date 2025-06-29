654c65fe186dc61e13d0d59819d8b732134b3c07
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-29 10:09:17 +0530

    Update alg.py

--- diversify/alg/alg.py
+++ diversify/alg/alg.py
@@ -1,10 +1,13 @@
 from .algs.diversify import Diversify
+
+__all__ = ['Diversify', 'get_algorithm_class']  # Add this line
+
 ALGORITHMS = [
     'diversify'
 ]
 
-
 def get_algorithm_class(algorithm_name):
+    """Factory function for algorithm selection"""
     if algorithm_name not in ALGORITHMS:
         raise NotImplementedError(
             "Algorithm not found: {}".format(algorithm_name))
