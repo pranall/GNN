0d217db26d611ed8e7a5b03d23f4fad6c31fc424
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-29 09:59:10 +0530

    Update __init__.py

--- diversify/datautil/actdata/__init__.py
+++ diversify/datautil/actdata/__init__.py
@@ -1,2 +1,4 @@
+from .cross_people import ActList, load_datasets
 from .util import act_to_graph_transform
-from .cross_people import *
+
+__all__ = ['ActList', 'load_datasets', 'act_to_graph_transform']
