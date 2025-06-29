c54459aab6facb3ffb5d93974b67de914580ff2d
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-29 20:05:23 +0530

    Update __init__.py

--- diversify/alg/__init__.py
+++ diversify/alg/__init__.py
@@ -1,7 +1,3 @@
 from .alg import *
 from .modelopera import *
 from .opt import *
-from .alg      import get_algorithm_class
-from .modelopera import get_fea, accuracy
-from .opt      import get_optimizer
-__all__ = ["get_algorithm_class", "get_fea", "accuracy", "get_optimizer"]
