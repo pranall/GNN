a690e37f70ade7b0013c9e19f955a4bc1c47c8b2
Pranal <89183452+pranall@users.noreply.github.com>
2025-06-29 20:10:03 +0530

    Update __init__.py

--- diversify/gnn/__init__.py
+++ diversify/gnn/__init__.py
@@ -1,4 +1,2 @@
-from .temporal_gcn import TemporalGCN
-from .graph_builder import build_graph_from_emg
-
-__all__ = ['TemporalGCN', 'build_graph_from_emg']
+from .graph_builder import *
+from .temporal_gcn import *
